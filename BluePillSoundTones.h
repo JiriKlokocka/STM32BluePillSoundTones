13ca8cae9e2d56740082e4d8a4088915a11942cd
Kangaroocz <jk@kangaroo-recording.com>
2018-11-16 13:32:56 +0100

    Add files via upload

---
+++ BluePillSoundTones.h
@@ -0,0 +1,54 @@
+/*
+ * BluePillSoundTones.h
+ *
+ *  Created on: 15. 11. 2018
+ *      Author: KlokockaJir
+ */
+
+#ifndef BLUEPILLSOUNDTONES_H_
+#define BLUEPILLSOUNDTONES_H_
+
+
+#include "Arduino.h"
+#include "stm32f103.h"
+
+
+#define DACSAMPLINGRATE_HZ    20000
+#define TIMER_INTTERUPT_US    1000000UL / DACSAMPLINGRATE_HZ
+#define DMABUFFERLENGTH 100
+#define SPEAKERPIN PB0            // speaker pin, be sure that it corresponds to the DMA pin setup in the next line
+#define PIN_PB 0                  // speaker pin, you can choose 0..7, 0 means PB0, 1 means PB1 ...
+#define PORTB_PINMASK (1<<PIN_PB)
+
+
+typedef struct
+{
+  uint16_t attackTime; //in milliseconds
+  uint8_t attackBeginVol; //in % of Volume
+  uint8_t sustainVol; //in % of Volume
+  uint8_t decayFinalVol; //in % of Volume
+  uint8_t decayTime; //in % tone length
+
+} toneShape;
+
+class BPSound {
+public:
+	static void DACsampleRateHandler();
+	void startDMA();
+	void BPSoundBegin();
+	void startTimer();
+	void writeDac( uint32_t sollwert );
+	void sweepTone(uint16_t freqv1,uint16_t freqv2, uint16_t lengthStep, uint8_t volume );
+	void playTone(toneShape shape, uint16_t freqv, uint16_t length, uint8_t volume );
+	void setVolume(float vol);
+	void toneStart(uint16_t freqv);
+	void toneStop();
+	void sineTone(uint16_t freqv, uint16_t dur, uint8_t volume);
+	void checkSampleRate();
+	void setSine();
+	void setSquare();
+
+};
+
+#endif /* BLUEPILLSOUNDTONES_H_ */
+
---
+++ STM32_BluePillSOund.ino
@@ -0,0 +1,166 @@
+
+
+/*
+ * Simple sound and tone shapes generator based on ChrisMicro's BluePillSound library:
+ * https://github.com/ChrisMicro/BluePillSound
+ * especially on this sketch:
+ * https://github.com/ChrisMicro/BluePillSound/tree/master/_082_dmaSigmaDeltaTimerOscillator
+ *
+ */
+
+#include "stm32f103.h"
+#include "BluePillSoundTones.h"
+#include "Tones.h"
+
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) //determine number of elements in a field
+
+toneShape piano, flute, reverse;
+
+int melody1[] = { NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_G6, NOTE_A6, NOTE_B6, NOTE_C7 };
+int melody11[] = { NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6 };
+int melody12[] = { NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5 };
+int melody2[] = { NOTE_C7, NOTE_B6, NOTE_A6, NOTE_G6, NOTE_F6, NOTE_E6, NOTE_D6,NOTE_C6 };
+int melody21[] = { NOTE_C6, NOTE_B5, NOTE_A5, NOTE_G5, NOTE_F5, NOTE_E5, NOTE_D5,NOTE_C5 };
+
+int melodyXX[] = { NOTE_C6, NOTE_G5, NOTE_G5, NOTE_A5, NOTE_G5, 0, NOTE_B5, NOTE_C6 };
+
+int melodyXXnoteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4 };
+
+
+BPSound sound;
+
+void setup()
+{
+	sound.BPSoundBegin();
+
+
+
+	/* Tone shape definitions
+	 *
+	 *
+	 * Attack      Sustain      Decay
+	 *   \
+	 *    \
+	 *     \
+	 *      _ _ _ _ _ _ _ _ _ _
+	 *                          \
+	 *                            \
+	 *                              \
+	 *
+	 */
+
+	piano.attackTime = 50; 		//Absolute in milliseconds
+	piano.attackBeginVol = 100;	// % of tone volume
+	piano.sustainVol = 60; 		// % of tone volume
+	piano.decayTime = 90; 		// % of tone volume
+	piano.decayFinalVol = 10; 	// % of tone time resting after Attack
+
+	flute.attackTime = 60; //ms
+	flute.attackBeginVol = 00; //%
+	flute.sustainVol = 80; //%
+	flute.decayTime = 10; //%
+	flute.decayFinalVol = 10; //%
+
+	reverse.attackTime = 100; //ms
+	reverse.attackBeginVol = 100; //%
+	reverse.sustainVol = 10; //%
+	reverse.decayTime = 90; //%
+	reverse.decayFinalVol = 100; //%
+}
+
+
+
+void loop()
+{
+	int i;
+	sound.setSine(); //Sine wave
+	//Just tone (Frequency in Hz, length in milliseconds, volume in %)
+	sound.sineTone(600, 1000, 100);
+	delay(300);
+
+	sound.setSquare();  //Square wave
+	sound.sineTone(600, 1000, 100);
+	sound.setSine();
+
+	delay(300);
+
+	//Shaped tone (Tone shape, Frequency in Hz, length in milliseconds, volume in %)
+	sound.playTone(piano, 1000, 1000, 100.0 );
+	delay(300);
+	sound.playTone(flute, 800, 1000, 100.0 );
+	delay(300);
+	sound.playTone(reverse, 600, 1000, 100.0 );
+	delay(300);
+	//Tone sweep (Frequency FROM in Hz, Frequency TO in Hz, step in microseconds, volume in %;
+	sound.sweepTone(10,5000, 200, 100 );
+	sound.sweepTone(5000,10, 80, 100 );
+	sound.sweepTone(10,5000, 80, 100 );
+
+	delay(300);
+
+	for(int i = 0; i < NELEMS(melody1);i++)
+	{
+		//Shaped tone (Tone shape, Frequency in Hz, length in milliseconds, volume in %)
+		sound.playTone(piano, melody1[i], 300, 100.0 );
+	}
+	delay(500);
+	for(int i = 0; i < NELEMS(melody21);i++)
+	{
+		//Shaped tone (Tone shape, Frequency in Hz, length in milliseconds, volume in %)
+		sound.playTone(flute, melody21[i], 300, 100.0 );
+	}
+
+	delay(500);
+	for(int i = 0; i < NELEMS(melody1);i++)
+	{
+		//Shaped tone (Tone shape, Frequency in Hz, length in milliseconds, volume in %)
+		sound.playTone(reverse, melody1[i], 500, 100.0 );
+	}
+
+	delay(300);
+
+
+	sound.setSquare();
+	for(int i = 0; i < NELEMS(melodyXX);i++)
+	{
+		//Shaped tone (Tone shape, Frequency in Hz, length in milliseconds, volume in %)
+		int noteDuration = 1000 / melodyXXnoteDurations[i];
+		sound.playTone(piano, melodyXX[i], noteDuration, 100.0 );
+	}
+	sound.setSine();
+
+	//Slow sirene
+	for(i = 0; i<2;i++) {
+		//Tone sweep (Frequency FROM in Hz, Frequency TO in Hz, step in microseconds, volume in %;
+		sound.sweepTone(400,1500, 600, 100 );
+		sound.sweepTone(1500,400, 600, 100 );
+	}
+
+	//Fast sirene
+	for(i = 0; i<12;i++) {
+		//Tone sweep (Frequency FROM in Hz, Frequency TO in Hz, step in microseconds, volume in %;
+		sound.sweepTone(500,2000, 45, 100 );
+		sound.sweepTone(2000,500, 45, 100 );
+	}
+
+	delay(300);
+
+	sound.setSquare();
+
+	//Grrr Grrrr
+	delay(100 );
+	for(i = 0; i<60;i++) {
+		sound.sineTone(700, 10, 100);
+		if (i == 30)
+			delay(100);
+		else
+			delay(5);
+	}
+
+	sound.setSine();
+	delay(300);
+
+
+
+
+}
